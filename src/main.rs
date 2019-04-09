ab159fc8231420ba6dc4fc183e14fd5024fac69c
Matthias Krüger <matthias.krueger@famsik.de>
2019-04-09 12:38:30 +0200

    make it work

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@ fn main() {
     let cfg = App::new("webtail").arg(Arg::with_name("URL")).get_matches();
     let url = cfg.value_of("URL").unwrap_or("" /* default */);
 
+    let REFRESH_RATE = std::time::Duration::from_secs(10);
+
     println!("getting: {}", url);
+
+    let mut content_old = String::new();
 
     loop {
         let mut req = reqwest::get(url).expect("Could not get url");
         let content = req.text().unwrap();
 
-        for i in content.lines() {
-            println!("{}", i);
-        }
+        let line_diff = content.lines().count() - content_old.lines().count();
+        let old_lines = content.lines().count() - line_diff;
+        // only print new lines
+        content
+            .lines()
+            .skip(old_lines)
+            .for_each(|line| println!("{}", line));
 
-        std::thread::sleep(std::time::Duration::from_secs(5));
+        std::thread::sleep(REFRESH_RATE);
+        content_old = content;
     }
-    //println!("{:?}", content);
 }
