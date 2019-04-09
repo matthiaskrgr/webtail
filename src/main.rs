553e21b5ea714946dea33b61fef8a2f2ed538ae7
Matthias Krüger <matthias.krueger@famsik.de>
2019-04-09 12:41:03 +0200

    format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
-use reqwest::get;
+use clap::{App, Arg};
 
 fn main() {
     let cfg = App::new("webtail").arg(Arg::with_name("URL")).get_matches();
     let url = cfg.value_of("URL").unwrap_or("" /* default */);
 
-    let REFRESH_RATE = std::time::Duration::from_secs(10);
+    let refresh_rate = std::time::Duration::from_secs(10);
 
     println!("getting: {}", url);
 
@@ -23,7 +22,7 @@ fn main() {
             .skip(old_lines)
             .for_each(|line| println!("{}", line));
 
-        std::thread::sleep(REFRESH_RATE);
+        std::thread::sleep(refresh_rate);
         content_old = content;
     }
 }
